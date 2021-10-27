bed1454632c88e8203c3c43c8c2a760eec13ec8f
tanakayayoi <march.yayoi31@gmail.com>
2021-10-27 12:28:56 +0000

    [Fix]sort_by favorites

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
-  # 投稿をいいね順に５つ表示
+  # 投稿をいいね順に表示
   def top
-    @posts = Post.includes(:favorited_users).limit(5).sort_by {|post| post.favorites.count}.reverse
+    @posts = Post.includes(:favorited_users).sort_by {|post| post.favorites.count}.reverse
     @tag_list = Tag.all
   end
 end
