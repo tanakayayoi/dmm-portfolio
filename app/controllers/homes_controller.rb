7d59291bc03da187f82c338b31a2e0947eef2823
tanakayayoi <march.yayoi31@gmail.com>
2021-11-27 12:48:01 +0000

    [Fix]homes_controller

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   # 投稿をいいね順に表示
   def top
-    @posts = Post.includes(:favorited_users).sort_by {|post| post.favorites.count}.reverse
+    @posts = Post.left_joins(:favorites).group(:id).order('count(post_id) desc').limit(5)
     @tag_list = Tag.all
   end
 end
